ee908ed5be4111ae7cb61798b9df651d6e071a9f
MelvinBrink <48125702+MelvinBrink@users.noreply.github.com>
2024-03-18 19:24:58 +0100

    Backup before change

--- Game/Plugins/MBNavigation/Source/MBNavigation/Private/NavMeshUpdater.cpp
+++ Game/Plugins/MBNavigation/Source/MBNavigation/Private/NavMeshUpdater.cpp
@@ -6,6 +6,19 @@
 DEFINE_LOG_CATEGORY(LogNavMeshUpdater)
 
 
+struct FAxisCheck
+{
+	// For if the axis on first/last node is exactly on a the current rounded-axis for that loop, and we have not yet checked it.
+	bool bHasCheckedFirst = false;
+	bool bHasCheckedLast = false;
+
+	// To know if we are able to skip over already iterated parts.
+	bool bCanSkip = false;
+
+	// For determining if there are nodes in-between the Min/Max of a coordinate.
+	uint16 DiffCriteria = 1;
+};
+
 
 void FNavMeshUpdater::UpdateStatic(const TArray<TBoundsPair<>>& BeforeAfterBoundsPairs)
 {
@@ -58,82 +71,97 @@ void FNavMeshUpdater::UpdateStatic(const TArray<TBoundsPair<>>& BeforeAfterBound
 			const TBounds<F3DVector10> PrevMortonBounds = LocalBoundsPair.Previous;
 			const TBounds<F3DVector10> CurrMortonBounds = LocalBoundsPair.Current;
 
-			// For determining if there are nodes in-between the Min/Max of a coordinate.
-			uint8 DiffXCriteria = 1;
-			uint8 DiffYCriteria = 1;
-			uint8 DiffZCriteria = 1;
-
+			// XYZ
+			std::array<FAxisCheck, 3> FAxisChecks;
+			
 			// Used to skip parts that have already been checked.
 			F3DVector10 StartSkip = F3DVector10();
 			F3DVector10 EndSkip = F3DVector10();
-			bool bSkipX = false, bSkipY = false, bSkipZ = false;
 
+			/* --- Currently only does the PrevBounds for testing --- */
+			
 			// Loop through all layers.
+			// todo start with the widest axis, so that you cover the most space in one go?
 			TArray<const uint32> MortonCodesToCheck;
-			for (uint8 LayerIndex = 0; LayerIndex<=9; ++LayerIndex)
+			for (auto AxisCheck : FAxisChecks)
 			{
-				const uint8 Shift = 9-LayerIndex;
-				const uint16 MortonOffset = FNavMeshData::MortonOffsets[LayerIndex];
-
-				// Floor value to nearest multiple of the current node-size for this layer.
-				const F3DVector10 ShiftedMin = PrevMortonBounds.Min >> Shift;
-				const F3DVector10 ShiftedMax = PrevMortonBounds.Max >> Shift;
-				
-				// Get node-count difference between min-max for each XYZ coordinate of this layer. // todo check if we maybe need to add +1 because min/max are on same node?
-				const uint16 DiffX = ShiftedMin.X - ShiftedMax.X;
-				const uint16 DiffY = ShiftedMin.Y - ShiftedMax.Y;
-				const uint16 DiffZ = ShiftedMin.Z - ShiftedMax.Z;
-
-				// Round MortonBounds to the nearest multiple of 'node-size' for this layer.
-				const F3DVector10 RoundedMin = PrevMortonBounds.Min & FNavMeshData::MortonMasks[LayerIndex];
-				const F3DVector10 RoundedMax = PrevMortonBounds.Max & FNavMeshData::MortonMasks[LayerIndex];
-				
-				//
-				if(DiffX > DiffXCriteria)
+				for (uint8 LayerIndex = 0; LayerIndex<=9; ++LayerIndex)
 				{
-					// Get Morton-codes of the nodes BETWEEN the rounded Min and Max X axis, and for-each Y and Z axis INCLUDING their Min and Max.
+					const uint8 Shift = 9-LayerIndex;
+					const uint16 MortonOffset = FNavMeshData::MortonOffsets[LayerIndex];
+
+					// Floor value to nearest multiple of the current node-size for this layer.
+					const F3DVector10 ShiftedMin = PrevMortonBounds.Min >> Shift;
+					const F3DVector10 ShiftedMax = PrevMortonBounds.Max >> Shift;
 					
-					// Get each X coordinate between the rounded Min/Max. This EXCLUDES the first/last.
-					uint16 StartX = RoundedMin.X+MortonOffset;
-					if(bSkipX && StartX == StartSkip.X) StartX = EndSkip.X; // Skip immediately if starting X has already been checked.
-					for (uint16 X = StartX; X<RoundedMax.X; X+=MortonOffset)
+					// Get node-count difference between min-max for each XYZ coordinate of this layer.
+					const uint16 DiffX = ShiftedMin.X - ShiftedMax.X;
+					const uint16 DiffY = ShiftedMin.Y - ShiftedMax.Y;
+					const uint16 DiffZ = ShiftedMin.Z - ShiftedMax.Z;
+
+					// Round MortonBounds to the nearest multiple of 'node-size' for this layer.
+					const F3DVector10 RoundedMin = PrevMortonBounds.Min & FNavMeshData::MortonMasks[LayerIndex];
+					const F3DVector10 RoundedMax = PrevMortonBounds.Max & FNavMeshData::MortonMasks[LayerIndex];
+
+					// todo implement the static-depth
+					if(!AxisCheck.bHasCheckedFirst && RoundedMin == PrevMortonBounds.Min)
 					{
-						// Skip the parts on X that have already been checked in previous layers.
-						if(bSkipX)
+						AxisCheck.bHasCheckedFirst = true;
+						// Recurse through this first node on X.
+					}
+					if(!AxisCheck.bHasCheckedLast && RoundedMax == PrevMortonBounds.Max)
+					{
+						AxisCheck.bHasCheckedLast = true;
+						// Recurse through this last node on X.
+					}
+					
+					//
+					if(DiffX > AxisCheck.DiffCriteria)
+					{
+						// Get Morton-codes of the nodes BETWEEN the rounded Min and Max X axis, and for-each Y and Z axis INCLUDING their Min and Max.
+						
+						// Get each X coordinate between the rounded Min/Max. This EXCLUDES the first/last.
+						uint16 StartX = RoundedMin.X+MortonOffset;
+						if(AxisCheck.bCanSkip && StartX == StartSkip.X) StartX = EndSkip.X; // Skip immediately if starting X has already been checked.
+						for (uint16 X = StartX; X<RoundedMax.X; X+=MortonOffset)
 						{
-							if(X == StartSkip.X)
+							// Skip the parts on X that have already been checked in previous layers.
+							if(AxisCheck.bCanSkip)
 							{
-								X=EndSkip.X;
-								continue;
-							}
-							if(X < StartSkip.X)
-							{
-								StartSkip.X = X;
+								if(X == StartSkip.X)
+								{
+									X=EndSkip.X;
+									continue;
+								}
+								if(X < StartSkip.X)
+								{
+									StartSkip.X = X;
+								}
+								else
+								{
+									EndSkip.X = X+MortonOffset;
+								}
 							}
 							else
 							{
-								EndSkip.X = X+MortonOffset;
+								AxisCheck.bCanSkip = true;
+								StartSkip.X = X;
 							}
-						}
-						else
-						{
-							bSkipX = true;
-							StartSkip.X = X;
+							
+							// Get each Y/Z coordinate. This INCLUDES the first/last.
+							for (uint16 Y = RoundedMin.Y; Y<=RoundedMax.Y; Y+=MortonOffset)
+							{
+								for (uint16 Z = RoundedMin.Z; Z<=RoundedMax.Z; Z+=MortonOffset)
+								{
+									
+								}
+							}
 						}
 						
-						// Get each Y/Z coordinate. This INCLUDES the first/last.
-						for (uint16 Y = RoundedMin.Y; Y<=RoundedMax.Y; Y+=MortonOffset)
-						{
-							for (uint16 Z = RoundedMin.Z; Z<=RoundedMax.Z; Z+=MortonOffset)
-							{
-								
-							}
-						}
+						// Next DiffXCriteria should be the current-diff multiplied by two plus one.
+						// We double the current-diff because the next layer has nodes halve the size of the current one, so it also holds twice as many nodes, which we should all skip.
+						AxisCheck.DiffCriteria = (DiffX << 1)+1; // +1 because the difference needs to be at-least two for there to be a node in-between the Start/End.
 					}
-					
-					// Next DiffXCriteria should be the current-diff multiplied by two plus one.
-					// We double the current-diff because the next layer has nodes halve the size of the current one, so it also holds twice as many nodes, which we should all skip.
-					DiffXCriteria = (DiffX << 1)+1; // +1 because the difference needs to be at-least two for there to be a node in-between the Start/End.
 				}
 			}
 		}
