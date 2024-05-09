04e2b0ebe038083d1ea890eab1c95569f484d9b6
MelvinBrink <48125702+MelvinBrink@users.noreply.github.com>
2024-05-09 20:12:12 +0200

    Removed unused code

--- Game/Plugins/MBNavigation/Source/MBNavigation/Public/MBNavigation/NavMesh/Updater.h
+++ Game/Plugins/MBNavigation/Source/MBNavigation/Public/MBNavigation/NavMesh/Updater.h
@@ -22,16 +22,6 @@ public:
 	void UpdateStatic(const std::vector<TBoundsPair<F3DVector32>>& BoundsPairs);
 
 private:
-	// Hashmap storing chunks with a set of morton-codes.
-	typedef ankerl::unordered_dense::map<const FChunk*, std::unordered_set<uint_fast32_t>> FChunkMortonSetMap;
-
-	// Map storing morton-code/direction pairs. Used to associate a direction with a node where the direction is the neighbours which we want to update for this node.
-	typedef ankerl::unordered_dense::map<uint_fast32_t, uint8> FNodeDirectionMap;
-	// Map associating a FNodeDirectionMap with a chunk. Used to update the node relations.
-	typedef ankerl::unordered_dense::map<const FChunk*, FNodeDirectionMap> FNodeRelationUpdateMap;
-	void AddRelationToUpdate(const FChunk* Chunk, const uint_fast32_t MortonCode, const uint8 UpdateDirection);
-	
-	static std::unordered_set<uint_fast32_t>& GetMortonSetForChunk(const FChunk* Chunk, FChunkMortonSetMap& ChunkMortonSetMap);
 	template<typename Func> void ForEachChunkIntersection(const TBounds<F3DVector32>& Bounds, const uint8 LayerIdx, Func Callback);
 	
 	bool StartClearUnoccludedChildrenOfNode(const FChunk* Chunk, const uint_fast32_t NodeMortonCode, const uint8 LayerIdx);
@@ -39,11 +29,7 @@ private:
 	bool StartReRasterizeNode(const FChunk* Chunk, const uint_fast32_t NodeMortonCode, const uint8 LayerIdx);
 	
 	void UnRasterize(const FChunk* Chunk, const std::unordered_set<uint_fast32_t>& NodeMortonCodes, const uint8 LayerIdx);
-	void UpdateRelations(const TBounds<F3DVector32>& CurrentBounds, const std::vector<TBounds<F3DVector32>>& PreviousRemainders, FChunkMortonSetMap& UpdatedMortonCodesMap, const uint8 LayerIdx);
-	void UpdateRelationsForNodes(const FChunk* Chunk, const std::unordered_set<uint_fast32_t>& MortonCodes);
 	
 	FNavMeshPtr NavMeshPtr;
 	const UWorld* World;
-
-	FNodeRelationUpdateMap NodeRelationUpdateMap;
 };
