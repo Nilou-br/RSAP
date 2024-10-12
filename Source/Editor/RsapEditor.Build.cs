1c72d4b0256d55da0843097949fade1563f7fddb
MelvinBrink <48125702+MelvinBrink@users.noreply.github.com>
2024-10-12 20:07:33 +0200

    Moved .git to plugin folder

---
+++ Source/Editor/RsapEditor.Build.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using UnrealBuildTool;
+
+public class RsapEditor : ModuleRules
+{
+	public RsapEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core"
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"Slate",
+				"SlateCore",
+				"ToolMenus",
+				"LevelEditor",
+				"UMG",
+				"Blutility",
+				"UnrealEd",
+				"LevelEditor",
+				"Projects",
+				"EditorStyle",
+				"EditorSubsystem",
+				"RsapGame"
+			}
+		);
+	}
+}
--- Source/RSAP_Project/RSAP_Project.cpp
+++
@@ -1,4 +0,0 @@
-#include "RSAP_Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RSAP_Project, "RSAP_Project" );
